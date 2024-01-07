28a312ec03ca5e280fecca8e9d883b385b76ae7f
Mikita <tetropak555666@gmail.com>
2024-01-07 17:46:02 +0900

    fix

--- app/api/public/news/route.tsx
+++ app/api/public/news/route.tsx
@@ -9,10 +9,12 @@ export async function GET(req: Request) {
     const take = searchParams.get("take");
     const id = searchParams.get("id");
     const random = searchParams.get("random") || undefined;
-    const offset = searchParams.get("offset") || undefined;
-    const limit = searchParams.get("limit") || undefined;
+    const offset = searchParams.get("offset") || "0";
+    const limit = searchParams.get("limit") || "100";
 
     const takeNumber = take ? parseInt(take, 10) : undefined;
+    const offsetNumber = parseInt(offset, 10);
+    const limitNumber = parseInt(limit, 10);
 
     if (id) {
       const news = await prismadb.news.findUnique({
@@ -47,24 +49,23 @@ export async function GET(req: Request) {
         include: { images: true },
       });
       return NextResponse.json(randomArticles);
-    } else if (offset && limit) {
-      const offsetNumber = parseInt(offset, 10);
-      const limitNumber = parseInt(limit, 10);
+    } else {
       const news = await prismadb.news.findMany({
         orderBy: {
-          createdAt: "desc",
+          createdAt: "desc", // Order by createdAt field in descending order
         },
         include: { images: true },
         skip: offsetNumber,
         take: limitNumber,
       });
       return NextResponse.json(news);
-    } else {
-      const news = await prismadb.news.findMany({
-        include: { images: true },
-      });
-      return NextResponse.json(news);
     }
+    // else {
+    //   const news = await prismadb.news.findMany({
+    //     include: { images: true },
+    //   });
+    //   return NextResponse.json(news);
+    // }
   } catch (error) {
     console.log("[NEWS_GET]", error);
     return new NextResponse("Internal error", { status: 500 });
