a77632e9b5e5d8ab296d3f742f52d769ca08dcb2
Mikita <tetropak555666@gmail.com>
2024-01-07 14:59:46 +0900

    fix

--- app/api/public/getCountOf/route.tsx
+++ app/api/public/getCountOf/route.tsx
@@ -1,3 +1,4 @@
+import { Request } from "express";
 import { NextResponse } from "next/server";
 import prismadb from "@/lib/prismadb";
 
@@ -5,16 +6,28 @@ export async function GET(req: Request) {
   try {
     const { searchParams } = new URL(req.url);
     const target = searchParams.get("target") || undefined;
-    const allowedFields = Object.keys(prismadb);
-    if (target && allowedFields.includes(target)) {
-      const count = await prismadb[target].count();
+
+    if (target === "articles") {
+      const count = await prismadb.articles.count();
+      return NextResponse.json(count);
+    } else if (target === "news") {
+      const count = await prismadb.news.count();
+      return NextResponse.json(count);
+    } else if (target === "theme") {
+      const count = await prismadb.theme.count();
+      return NextResponse.json(count);
+    } else if (target === "koreanPhrase") {
+      const count = await prismadb.koreanPhrase.count();
+      return NextResponse.json(count);
+    } else if (target === "tag") {
+      const count = await prismadb.tag.count();
       return NextResponse.json(count);
     } else {
       console.error(`Invalid or missing target: ${target}`);
       return new NextResponse("Bad Request", { status: 400 });
     }
   } catch (error) {
-    console.log("[ARTICLES_GET]", error);
+    console.error("[ARTICLES_GET]", error);
     return new NextResponse("Internal error", { status: 500 });
   }
 }
