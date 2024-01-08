6c344ada37620707254c585ac4510e669d9055ce
Mikita <tetropak555666@gmail.com>
2024-01-08 16:08:55 +0900

    api

--- app/api/public/tags/route.tsx
+++ app/api/public/tags/route.tsx
@@ -2,13 +2,35 @@ import { NextResponse } from "next/server";
 
 import prismadb from "@/lib/prismadb";
 
-export async function GET() {
+export async function GET(req: Request) {
   try {
-    const tags = await prismadb.tag.findMany({
-      include: { images: true },
-    });
+    const { searchParams } = new URL(req.url);
+    const tagLabel = searchParams.get("tagLabel") || undefined;
+    const tagId = searchParams.get("tagId") || undefined;
 
-    return NextResponse.json(tags);
+    if (tagId) {
+      const tag = await prismadb.tag.findUnique({
+        where: {
+          id: tagId,
+        },
+        include: { images: true },
+      });
+      return NextResponse.json(tag);
+    } else if (tagLabel) {
+      const tag = await prismadb.tag.findFirst({
+        where: {
+          label: tagLabel,
+        },
+        include: { images: true },
+      });
+      return NextResponse.json(tag);
+    } else {
+      const tags = await prismadb.tag.findMany({
+        include: { images: true },
+      });
+
+      return NextResponse.json(tags);
+    }
   } catch (error) {
     console.log("[TAG_GET]", error);
     return new NextResponse("Internal error", { status: 500 });
