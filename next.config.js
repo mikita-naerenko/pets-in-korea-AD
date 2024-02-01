aae43e962e6cb04ec4368ff5033f55b2cbd971fd
Mikita <tetropak555666@gmail.com>
2024-02-01 20:32:03 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@
 //         domains: ["res.cloudinary.com"]
 //       }
 // }
+// http://localhost:3001
 
 const nextConfig = {
   async headers() {
@@ -13,7 +14,7 @@ const nextConfig = {
               source: "/api/public/:path*",
               headers: [
                   { key: "Access-Control-Allow-Credentials", value: "true" },
-                  { key: "Access-Control-Allow-Origin", value: "https://www.pets-in-korea.com" }, // replace this your actual origin
+                  { key: "Access-Control-Allow-Origin", value: "https://pets-in-korea.com" }, // replace this your actual origin
                   { key: "Access-Control-Allow-Methods", value: "OPTIONS, GET, DELETE, PATCH, POST, PUT" },
                   { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
               ]
